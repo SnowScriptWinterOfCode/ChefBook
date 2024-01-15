220a40edba5ed5759dd7df21956f7b4b0c05dd1e
anushree <aushreebondia@gmail.com>
2024-01-15 22:17:46 +0530

    added the comments section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
